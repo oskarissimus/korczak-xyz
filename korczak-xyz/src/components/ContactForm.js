42702f2eaa69befec4032658f0510400c1457bf9
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-07 17:34:24 +0100

    contact form is working fine

--- korczak-xyz/src/components/ContactForm.js
+++ korczak-xyz/src/components/ContactForm.js
@@ -1,30 +1,70 @@
-import React from "react";
+import React, { useEffect, useCallback } from 'react';
+import {
+    GoogleReCaptchaProvider,
+    useGoogleReCaptcha
+} from 'react-google-recaptcha-v3';
+
+
+const YourReCaptchaComponent = ({ setToken }) => {
+    const { executeRecaptcha } = useGoogleReCaptcha();
+
+    // Create an event handler so you can call the verification on button click event or form submit
+    const handleReCaptchaVerify = useCallback(async () => {
+        if (!executeRecaptcha) {
+            console.log('Execute recaptcha not yet available');
+            return;
+        }
+
+        const token = await executeRecaptcha('yourAction');
+        setToken(token);
+        // Do whatever you want with the token
+    }, [executeRecaptcha, setToken]);
+
+    // You can use useEffect to trigger the verification as soon as the component being loaded
+    useEffect(() => {
+        handleReCaptchaVerify();
+    }, [handleReCaptchaVerify]);
+
+    return <></>;
+};
+
 
 export default function ContactForm() {
-    function handleSubmit(event) {
+
+
+    const [token, setToken] = React.useState(null);
+
+    async function handleSubmit(event) {
         event.preventDefault();
         const form = event.target.form;
         const data = new FormData(form);
+        data.append("g-recaptcha-response", token);
+        console.log(token);
+        console.log(data);
         const url = "https://getform.io/f/da77d728-d0c5-4090-9a77-799464d888ff";
-        fetch(url, {
-            method: "POST",
-            body: data,
-            headers: {
-                'Accept': 'application/json'
-            }
-        }).then(response => {
+        try {
+            await fetch(url, {
+                method: "POST",
+                body: data,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
             alert("Thank you for your message!");
             form.reset()
-        }).catch(error => {
-            alert("Something went wrong!");
-        });
+        } catch (error) {
+            console.log(error);
+        }
     }
+
     return (
         <form
             accept-charset="UTF-8"
             className="flex flex-col gap-4"
         >
-
+            <GoogleReCaptchaProvider reCaptchaKey="6Lf-apkaAAAAAHYePzpdMhbG_u7njBLSVE5rOON5">
+                <YourReCaptchaComponent setToken={setToken} />
+            </GoogleReCaptchaProvider>
             <label className="block">
                 <span className="font-bold">
                     Name
@@ -49,7 +89,6 @@ export default function ContactForm() {
             </label>
 
 
-            <input type="hidden" id="captchaResponse" name="g-recaptcha-response" />
             <input type="hidden" name="_gotcha" style={{ display: "none" }} />
             <button
                 type="submit"
@@ -59,6 +98,7 @@ export default function ContactForm() {
                 Submit
             </button>
         </form>
+
     )
 }
 
