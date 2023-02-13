3647b8edeee359cfbaaa6abc38e7546774036625
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-13 20:51:51 +0100

    move DarkMode Switch to separate file

---
+++ korczak-xyz/src/components/DarkModeSwitch.js
@@ -0,0 +1,29 @@
+import React, { useCallback, useEffect, useContext } from 'react'
+import { ThemeContext } from './ThemeContext'
+
+
+export default function DarkModeSwitch() {
+    const { theme, setTheme } = useContext(ThemeContext)
+    useEffect(() => {
+        if (theme === 'dark') {
+            localStorage.theme = "dark"
+            document.documentElement.classList.add("dark")
+        } else {
+            localStorage.theme = "light"
+            document.documentElement.classList.remove("dark")
+        }
+    }, [theme])
+
+    const toggleTheme = useCallback(() => {
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+    }, [theme, setTheme])
+
+
+    return (
+        <label className="fixed inline-flex cursor-pointer bottom-20 left-20">
+            <input type="checkbox" className="sr-only peer" onChange={toggleTheme} checked={theme === 'dark'} />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ml-3 text-sm font-medium">Dark mode</span>
+        </label>
+    )
+}
--- korczak-xyz/src/components/Layout.js
+++ korczak-xyz/src/components/Layout.js
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect, useState, useContext } from 'react'
+import React, { useState } from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import { ThemeContext, getTheme } from './ThemeContext'
+import DarkModeSwitch from './DarkModeSwitch'
 
 
 export default function Layout({ children }) {
@@ -22,30 +23,3 @@ export default function Layout({ children }) {
 }
 
 
-
-
-function DarkModeSwitch() {
-    const { theme, setTheme } = useContext(ThemeContext)
-    useEffect(() => {
-        if (theme === 'dark') {
-            localStorage.theme = "dark"
-            document.documentElement.classList.add("dark")
-        } else {
-            localStorage.theme = "light"
-            document.documentElement.classList.remove("dark")
-        }
-    }, [theme])
-
-    const toggleTheme = useCallback(() => {
-        setTheme(theme === 'dark' ? 'light' : 'dark')
-    }, [theme, setTheme])
-
-
-    return (
-        <label className="fixed inline-flex cursor-pointer bottom-20 left-20">
-            <input type="checkbox" className="sr-only peer" onChange={toggleTheme} checked={theme === 'dark'} />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span className="ml-3 text-sm font-medium">Dark mode</span>
-        </label>
-    )
-}
