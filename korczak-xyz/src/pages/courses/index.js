fdf2d1f22660a612f5746ca755e38891100bc5af
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-13 23:07:03 +0100

    bw images for dark mode on courses page

--- korczak-xyz/src/pages/courses/index.js
+++ korczak-xyz/src/pages/courses/index.js
@@ -1,19 +1,22 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../components/Layout"
 import PageContent from "../../components/PageContent"
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import ThemeContext from "../../context/ThemeContext"
 
 
 
-function CourseSection({ title, image, description, link }) {
+function CourseSection({ title, imageColor, imageBW, description, link }) {
+    const { theme } = useContext(ThemeContext)
     return (
         <div className="flex flex-col gap-8" >
             <h3 className="text-4xl">
                 {title}
             </h3>
-            <GatsbyImage image={getImage(image)} alt={title} />
+            {theme === "dark" && <GatsbyImage image={getImage(imageBW)} alt={title} />}
+            {theme === "light" && <GatsbyImage image={getImage(imageColor)} alt={title} />}
             <p>{description}</p>
             <div>
                 <RedButton text="Dowiedz się więcej" link={link} />
@@ -35,24 +38,26 @@ export default function Courses({ data }) {
     const courses = [
         {
             title: "Zaawansowane narzędzia web-testera",
-            image: data.allFile.nodes.filter(node => node.name === "zaawansowane-narzedzia-web-testera-color")[0].childImageSharp.gatsbyImageData,
+            imageColor: data.allFile.nodes.find(node => node.name === "zaawansowane-narzedzia-web-testera-color"),
+            imageBW: data.allFile.nodes.find(node => node.name === "zaawansowane-narzedzia-web-testera-bw"),
             description: "Na kurs składa się 12 1-godzinnych indywidualnych spotkań z kursantem. Terminy spotkań są ustalane indywidualnie. Celem kursu jest zapoznanie z zaawansowanymi narzędziami stosowanymi w pracy web-testera. Oprócz materiału omawianego podczas spotkań prowadzący przedstawia również dodatkowe materiały do pracy własnej, które mają na celu utrwalić wiedzę i umiejętności kursanta.",
             link: "/courses/kurs-zaawansowane-narzedzia-web-testera/",
         },
         {
             title: "Wybrane aspekty sieciowe",
-            image: data.allFile.nodes.filter(node => node.name === "wybrane-aspekty-sieciowe-color")[0].childImageSharp.gatsbyImageData,
+            imageColor: data.allFile.nodes.find(node => node.name === "wybrane-aspekty-sieciowe-color"),
+            imageBW: data.allFile.nodes.find(node => node.name === "wybrane-aspekty-sieciowe-bw"),
             description: "Kurs ma na celu przybliżyć deweloperom wybrane aspekty sieciowe w praktyczny sposób. Zagadnienia są wybrane w taki sposób, aby kompleksowo omówić informacje potrzebne żeby skutecznie działać jako full-stack developer z pełną kontrolą nad sieciowością aplikacji.",
             link: "/courses/kurs-wybrane-aspekty-sieciowe/",
         },
         {
             title: "Programowanie backend w JS",
-            image: data.allFile.nodes.filter(node => node.name === "programowanie-backend-w-js-color")[0].childImageSharp.gatsbyImageData,
+            imageColor: data.allFile.nodes.find(node => node.name === "programowanie-backend-w-js-color"),
+            imageBW: data.allFile.nodes.find(node => node.name === "programowanie-backend-w-js-bw"),
             description: "Kurs jest przeznaczony dla rozpoczynających swoją przygodę z programowaniem. Kurs jest podzielony pięć części: programistyczne abecadło, zaawansowane aspekty języka, typescript, narzędzia backendowe w JS, projekt programistyczny. Każda z tych części jest opisana niżej. Na kurs składają się zarówno spotkania z mentorem jak i praca własna kursanta pomiędzy spotkaniami.",
             link: "/courses/kurs-programowanie-backend-w-js/",
         }
     ]
-    console.log(data)
     return (
         <Layout>
             <PageContent title="Courses">
