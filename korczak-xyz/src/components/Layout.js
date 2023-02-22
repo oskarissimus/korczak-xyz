f38f18bbe5b6563d2d49bf74b66cdb754921162e
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-22 14:07:56 +0100

    added responsive layout

--- korczak-xyz/src/components/Layout.js
+++ korczak-xyz/src/components/Layout.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import DarkModeSwitch from './DarkModeSwitch'
+import * as styles from '../styles/layout.module.css'
 
 
 export default function Layout({ children }) {
     return (
-        <div className="min-h-full font-default min-w-full flex flex-col items-center text-base text-justify leading-relaxed text-gray-900 dark:text-gray-300">
-            <div className='flex flex-col items-stretch w-full max-w-6xl gap-20'>
+        <div className={styles.outer_wrapper}>
+            <div className={styles.wrapper}>
                 <Navbar />
                 {children}
                 <Footer />
