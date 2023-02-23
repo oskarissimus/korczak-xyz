c98f33789d64ecb563a0744e48c28895a64c4b17
Oskar Korczak <oskar.korczak@protonmail.com>
2023-02-23 15:03:59 +0100

    moving back to tailwind in navbar

--- korczak-xyz/src/components/Navbar.js
+++ korczak-xyz/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import MenuItem from './MenuItem'
-import "../styles/Navbar.css"
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import DarkModeSwitch from './DarkModeSwitch'
@@ -18,19 +17,56 @@ const navigation = [
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false)
     return (
-        <div className='navbar_outer_wrapper'>
+        <div className='
+        flex
+        flex-col
+        border-b-red
+        fixed
+        top-0
+        left-0
+        w-full
+        z-30
+        md:max-w-6xl
+        bg-white
+        dark:bg-black
+        '>
             <div className='flex items-center justify-end'>
-                <DarkModeSwitch className='block sm:hidden' />
+                <DarkModeSwitch className='block md:hidden' />
                 <button
-                    className='flex sm:hidden'
+                    className='flex md:hidden'
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
                     height='2rem'
                     width='2rem'
                 >
-                    <FontAwesomeIcon icon={solid("burger")} className="navbar_burger" />
+                    <FontAwesomeIcon
+                        icon={solid("burger")}
+                        className="
+                        block
+                        md:hidden
+                        cursor-pointer
+                        text-3xl
+                        my-6
+                        mx-4
+                        "
+                    />
                 </button>
             </div>
-            <nav className={`navbar${isMenuOpen ? ' navbar-open' : ''}`}>
+            <nav className={`
+                flex
+                items-end
+                md:items-center
+                gap-4
+                ml-6
+                mr-6
+                md:mr-0
+                md:mt-4
+                mb-6
+                flex-col
+                md:flex
+                md:flex-row
+                ${isMenuOpen ? ' flex' : ' hidden'}
+                `
+            }>
                 <MenuItem name='korczak.xyz' href='/' className="sm:block hidden" />
                 <div className='grow hidden sm:block' />
                 {navigation.map(item => (
