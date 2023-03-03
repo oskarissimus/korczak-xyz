c3762c2033c4c763e99e6d2e911ce81ce1b2b3cf
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-03 01:32:34 +0100

    small 3-element gallery component

---
+++ korczak-xyz/src/components/Gallery.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+const staticImageProps = {
+    aspectRatio: 3 / 2,
+    style: { display: "block" },
+}
+
+const ImageContainer = ({ children }) => (
+    <div style={{ flexGrow: 1 }}>
+        {children}
+    </div>
+)
+
+export default function Papaj() {
+    return (
+        <div style={{ display: "flex", gap: 20 }}>
+            <ImageContainer>
+                <StaticImage
+                    src="../images/blog/led-matrix/nes-controller.jpg"
+                    alt="NES controller"
+                    {...staticImageProps}
+                />
+                NES controller
+            </ImageContainer>
+            <ImageContainer>
+                <StaticImage
+                    src="../images/blog/led-matrix/nespi.png"
+                    alt="NESPi case"
+                    {...staticImageProps}
+                />
+                NESPi case
+            </ImageContainer>
+            <ImageContainer>
+                <StaticImage
+                    src="../images/blog/led-matrix/raspberry-pi.jpg"
+                    alt="Raspberry Pi"
+                    {...staticImageProps}
+                />
+                Raspberry Pi
+            </ImageContainer>
+        </div>
+
+    )
+
+
+}
