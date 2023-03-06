ea0d832cae89857586159617e773d90c85fe72d6
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-06 18:12:02 +0100

    changing file structure for gallery for blog post

--- korczak-xyz/src/blog/led-matrix/Gallery/Gallery.js
+++ korczak-xyz/src/blog/led-matrix/Gallery/Gallery.js
@@ -1,23 +1,18 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import { ImageContainer } from "./ImageContainer";
 
 const staticImageProps = {
     aspectRatio: 3 / 2,
     style: { display: "block" },
-}
-
-const ImageContainer = ({ children }) => (
-    <div style={{ flexGrow: 1 }}>
-        {children}
-    </div>
-)
+};
 
 export default function Gallery() {
     return (
         <div style={{ display: "flex", gap: 20 }}>
             <ImageContainer>
                 <StaticImage
-                    src="../../images/blog/led-matrix/nes-controller.jpg"
+                    src="../../../images/blog/led-matrix/nes-controller.jpg"
                     alt="NES controller"
                     {...staticImageProps}
                 />
@@ -25,7 +20,7 @@ export default function Gallery() {
             </ImageContainer>
             <ImageContainer>
                 <StaticImage
-                    src="../../images/blog/led-matrix/nespi.png"
+                    src="../../../images/blog/led-matrix/nespi.png"
                     alt="NESPi case"
                     {...staticImageProps}
                 />
@@ -33,7 +28,7 @@ export default function Gallery() {
             </ImageContainer>
             <ImageContainer>
                 <StaticImage
-                    src="../../images/blog/led-matrix/raspberry-pi.jpg"
+                    src="../../../images/blog/led-matrix/raspberry-pi.jpg"
                     alt="Raspberry Pi"
                     {...staticImageProps}
                 />
