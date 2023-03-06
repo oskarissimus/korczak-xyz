937f9d053e1aa4181ee8fb8eba228af09e9905ef
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-06 18:15:09 +0100

    remove unused code

--- korczak-xyz/src/blog/led-matrix/Gallery/ImageContainer.js
+++ korczak-xyz/src/blog/led-matrix/Gallery/ImageContainer.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-export const staticImageProps = {
-    aspectRatio: 3 / 2,
-    style: { display: "block" },
-};
 export const ImageContainer = ({ children }) => (
     <div style={{ flexGrow: 1 }}>
         {children}
