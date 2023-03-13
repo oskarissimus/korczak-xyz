b86792467e5ff54c5172f34229b0d6cd49517228
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-13 18:25:44 +0100

    display date on blog posts page

--- korczak-xyz/src/pages/blog.js
+++ korczak-xyz/src/pages/blog.js
@@ -2,15 +2,45 @@ import React from "react";
 import Layout from "../components/Layout"
 import PageContent from "../components/PageContent"
 import { Seo } from "../components/Seo"
+import { graphql } from "gatsby"
+
 export const Head = () => (
     <Seo />
 )
-export default function Blog() {
+
+function formatDate(date) {
+    const d = new Date(date);
+    return d.toLocaleDateString();
+}
+
+export default function Blog({ data }) {
+    const posts = data.allMdx.nodes;
     return (
         <Layout>
             <PageContent title="Blog">
-                <p className="text-2xl">Blog posts will be here</p>
+                <ul>
+                    {posts.map(({ frontmatter: { slug, title, date } }) => (
+                        <li key={slug}>
+                            <a href={slug}>{title}</a>
+                            <span> - {formatDate(date)}</span>
+                        </li>
+                    ))}
+                </ul>
             </PageContent>
         </Layout>
     )
 }
+
+export const query = graphql`
+query BlogPosts {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          slug
+          title
+          date
+        }
+      }
+    }
+  }
+`
