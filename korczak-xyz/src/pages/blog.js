24392bd77f92cae47ab2994c695eb0b5c96f3a16
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-27 18:25:40 +0200

    improve looks of blog post list

--- korczak-xyz/src/pages/blog.js
+++ korczak-xyz/src/pages/blog.js
@@ -4,6 +4,7 @@ import PageContent from "../components/PageContent"
 import { Seo } from "../components/Seo"
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import "../styles/blog-list.css"
 
 export const Head = () => (
     <Seo />
@@ -19,17 +20,25 @@ export default function Blog({ data }) {
     return (
         <Layout>
             <PageContent title="Blog">
-                <ul>
-                    {posts.map(({ frontmatter: { slug, title, date, featuredImage } }) => (
-                        <li key={slug}>
-                            <GatsbyImage image={getImage(featuredImage)} alt={title} />
-                            <a href={slug}>{title}</a>
-                            <span> - {formatDate(date)}</span>
+                <ul className="flex flex-col gap-10">
+                    {posts.map(({ excerpt, frontmatter: { slug, title, date, featuredImage } }) => (
+                        <li key={slug} className="flex flex-col items-start gap-2">
+                            <h2 className="text-2xl font-bold">
+                                <a href={slug}>{title}</a>
+                            </h2>
+                            <div className="flex flex-row items-center gap-2">
+                                <GatsbyImage image={getImage(featuredImage)} alt={title} />
+                                <p>{excerpt}</p>
+
+
+                            </div>
+                            <hr className="w-full border-1 border-gray-500" />
+                            <span>{formatDate(date)}</span>
                         </li>
                     ))}
                 </ul>
             </PageContent>
-        </Layout>
+        </Layout >
     )
 }
 
@@ -37,6 +46,7 @@ export const query = graphql`
 query BlogPosts {
     allMdx(filter: {frontmatter: {published: {eq: true}}} sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
+        excerpt
         frontmatter {
           slug
           title
