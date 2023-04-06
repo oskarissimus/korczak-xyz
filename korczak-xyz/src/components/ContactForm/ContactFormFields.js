0127df59ecb1f93a0b9e5523e56359f474105dd7
Oskar Korczak <oskar.korczak@protonmail.com>
2023-04-06 12:44:52 +0200

    refactor: Integrate i18next in ContactFormFields component

--- korczak-xyz/src/components/ContactForm/ContactFormFields.js
+++ korczak-xyz/src/components/ContactForm/ContactFormFields.js
@@ -1,33 +1,37 @@
 import React from "react";
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 
 function FormField({ label, children }) {
+    const { t } = useTranslation();
     return <label className="block">
         <span className="font-bold">
-            {label}
+            {t(label)}
         </span>
         {children}
     </label>;
 }
 
 function TextField({ name, label, type, placeholder }) {
+    const { t } = useTranslation();
     return <FormField label={label}>
         <input
             type={type}
             name={name}
             className="block w-1/2 mt-1 dark:bg-[#1c1c1c] dark:border-[#444444]"
-            placeholder={placeholder}
+            placeholder={t(placeholder)}
             required="required"
         />
     </FormField>;
 }
 
 function TextareaField({ name, label, placeholder }) {
+    const { t } = useTranslation();
     return <FormField label={label}>
         <textarea
             name={name}
             className="block w-full mt-1 dark:bg-[#1c1c1c] dark:border-[#444444]"
             required="required"
-            placeholder={placeholder}
+            placeholder={t(placeholder)}
             rows="5"
         >
 
@@ -36,15 +40,18 @@ function TextareaField({ name, label, placeholder }) {
 }
 
 function MessageField() {
-    return <TextareaField name="message" label="Message" placeholder="Enter message" />
+    const { t } = useTranslation();
+    return <TextareaField name="message" label={t("Message")} placeholder={t("Enter message")} />
 }
 
 function NameField() {
-    return <TextField name="name" label="Name" type="text" placeholder="Enter name" />
+    const { t } = useTranslation();
+    return <TextField name="name" label={t("Name")} type="text" placeholder={t("Enter name")} />
 }
 
 function EmailField() {
-    return <TextField name="email" label="Email" type="email" placeholder="Enter email" />
+    const { t } = useTranslation();
+    return <TextField name="email" label={t("Email")} type="email" placeholder={t("Enter email")} />
 }
 
 function GotchaField() {
@@ -52,12 +59,13 @@ function GotchaField() {
 }
 
 function Button({ onClick }) {
+    const { t } = useTranslation();
     return <button
         type="submit"
         className="px-6 py-4 text-xl dark:bg-[#1c1c1c] hover:opacity-80 bg-[#eee] hover:opacity-80"
         onClick={onClick}
     >
-        Submit
+        {t("Submit")}
     </button>;
 }
 
