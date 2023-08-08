532bd0d2a2f577c5131d62e1b33a1768bf31c96e
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-08 23:10:08 +0200

    feat: Refactor Navbar component and add useNavbarData hook

--- korczak-xyz/src/components/Navbar.tsx
+++ korczak-xyz/src/components/Navbar.tsx
@@ -88,33 +88,37 @@ const MobileNavHeader: React.FC<MobileNavHeaderProps> = ({ isMenuOpen, setIsMenu
     );
 }
 
+function useNavbarData() {
+    const { languages, originalPath } = useI18next();
+    const { t } = useTranslation();
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const data = useStaticQuery(graphql`
+        query LogoQuery {
+            file(relativePath: { eq: "logo.png" }) {
+                childImageSharp {
+                    gatsbyImageData(width: 30)
+                }
+            }
+        }
+    `);
 
-
-interface NavbarProps { }
-
-const Navbar: React.FC<NavbarProps> = () => {
-    const { languages, originalPath } = useI18next()
-    const { t } = useTranslation()
-    const navigation = [
+    const imageData = data?.file?.childImageSharp?.gatsbyImageData as IGatsbyImageData | undefined;
+    const navigation = React.useMemo(() => [
         { name: t('Home'), to: '/' },
         { name: t('About'), to: '/about' },
         { name: t('Mentoring'), to: '/mentoring' },
         { name: t('Courses'), to: '/courses' },
         { name: t('Blog'), to: '/blog' },
-    ]
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false)
-    const data = useStaticQuery(graphql`
-    query LogoQuery {
-      file(relativePath: { eq: "logo.png" }) {
-        childImageSharp {
-          gatsbyImageData(width: 30)
-        }
-      }
-    }
-  `)
+    ], [t]);
 
-    const imageData = data?.file?.childImageSharp?.gatsbyImageData as IGatsbyImageData | undefined
+    return { languages, originalPath, isMenuOpen, setIsMenuOpen, imageData, navigation };
+}
 
+
+interface NavbarProps { }
+
+const Navbar: React.FC<NavbarProps> = () => {
+    const { languages, originalPath, isMenuOpen, setIsMenuOpen, imageData, navigation } = useNavbarData();
     return (
         <div className='flex flex-col border-b md:border-b-0 border-gray-400 sticky md:relative top-0 left-0 w-full z-30 md:max-w-6xl bg-white dark:bg-black mr-10 lg:text-lg'>
             <MobileNavHeader isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} imageData={imageData} />
