c4b6d59e4fa60bd4da949c2a59191447c8a36d8c
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-08 23:15:58 +0200

    Refactor Navbar component to remove unnecessary imports

--- korczak-xyz/src/components/Navbar/Navbar.tsx
+++ korczak-xyz/src/components/Navbar/Navbar.tsx
@@ -1,42 +1,11 @@
 import React from 'react'
-import MenuItem from '../MenuItem'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import DarkModeSwitch from '../DarkModeSwitch'
 import { graphql, useStaticQuery } from 'gatsby'
-import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
 import { useI18next, useTranslation } from 'gatsby-plugin-react-i18next'
 import MainNavMenu from './MainNavMenu'
+import MobileNavHeader from './MobileNavHeader'
 
 
-interface MobileNavHeaderProps {
-    isMenuOpen: boolean;
-    setIsMenuOpen: (value: boolean) => void;
-    imageData: IGatsbyImageData | undefined;
-}
-
-const MobileNavHeader: React.FC<MobileNavHeaderProps> = ({ isMenuOpen, setIsMenuOpen, imageData }) => {
-    return (
-        <div className='flex items-center justify-end ml-4'>
-            <div className='md:hidden flex gap-3'>
-                {imageData && <GatsbyImage image={imageData} alt='logo' />}
-                <MenuItem name='korczak.xyz' to='/' />
-            </div>
-            <div className='grow md:hidden' />
-            <DarkModeSwitch className='block md:hidden' />
-            <button
-                className='flex md:hidden'
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                aria-label='Menu'
-            >
-                <FontAwesomeIcon
-                    icon={icon({ name: 'burger', style: 'solid' })}
-                    className='block md:hidden cursor-pointer text-3xl my-6 mx-4'
-                />
-            </button>
-        </div>
-    );
-}
 
 function useNavbarData() {
     const { languages, originalPath } = useI18next();
