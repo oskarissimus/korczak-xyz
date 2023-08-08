3ec2396af6bc379955239c721f767525fd6a8c16
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-08 23:18:59 +0200

    refactor: Refactor navbar logic and remove unused code

--- korczak-xyz/src/components/Navbar/Navbar.tsx
+++ korczak-xyz/src/components/Navbar/Navbar.tsx
@@ -1,37 +1,9 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import { IGatsbyImageData } from 'gatsby-plugin-image'
-import { useI18next, useTranslation } from 'gatsby-plugin-react-i18next'
 import MainNavMenu from './MainNavMenu'
 import MobileNavHeader from './MobileNavHeader'
+import useNavbarData from '../../hooks/use-navbar-data'
 
 
-
-function useNavbarData() {
-    const { languages, originalPath } = useI18next();
-    const { t } = useTranslation();
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const data = useStaticQuery(graphql`
-        query LogoQuery {
-            file(relativePath: { eq: "logo.png" }) {
-                childImageSharp {
-                    gatsbyImageData(width: 30)
-                }
-            }
-        }
-    `);
-
-    const imageData = data?.file?.childImageSharp?.gatsbyImageData as IGatsbyImageData | undefined;
-    const navigation = React.useMemo(() => [
-        { name: t('Home'), to: '/' },
-        { name: t('About'), to: '/about' },
-        { name: t('Mentoring'), to: '/mentoring' },
-        { name: t('Courses'), to: '/courses' },
-        { name: t('Blog'), to: '/blog' },
-    ], [t]);
-
-    return { languages, originalPath, isMenuOpen, setIsMenuOpen, imageData, navigation };
-}
 
 
 interface NavbarProps { }
