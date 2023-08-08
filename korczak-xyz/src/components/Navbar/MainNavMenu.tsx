dff44fb1078c63559ccabf00588c0704c9a5aa73
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-08 23:35:47 +0200

    style: Refactor MainNavMenu component for improved readability and consistency

--- korczak-xyz/src/components/Navbar/MainNavMenu.tsx
+++ korczak-xyz/src/components/Navbar/MainNavMenu.tsx
@@ -1,48 +1,42 @@
-import React from 'react'
-import MenuItem from '../MenuItem'
-import DarkModeSwitch from '../DarkModeSwitch'
-import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
-import { Link } from 'gatsby-plugin-react-i18next'
-import Flag from '../Flag'
-
+import React from 'react';
+import MenuItem from '../MenuItem';
+import DarkModeSwitch from '../DarkModeSwitch';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
+import { Link } from 'gatsby-plugin-react-i18next';
+import Flag from '../Flag';
 
 interface MainNavMenuProps {
-    navigation: { name: string; to: string; }[];
+    navigation: { name: string; to: string }[];
     isMenuOpen: boolean;
     languages: string[];
     originalPath: string;
     imageData: IGatsbyImageData | undefined;
 }
 
-const MainNavMenu: React.FC<MainNavMenuProps> = ({ navigation, isMenuOpen, languages, originalPath, imageData }) => {
+const MainNavMenu: React.FC<MainNavMenuProps> = ({
+    navigation,
+    isMenuOpen,
+    languages,
+    originalPath,
+    imageData,
+}) => {
     return (
         <nav
             className={`
-            flex
-            items-end
-            md:items-center
-            gap-4
-            ml-6
-            mr-6
-            md:mr-0
-            md:mt-4
-            mb-6
-            flex-col
-            md:flex
-            md:flex-row
-            ${isMenuOpen ? ' flex' : ' hidden'}
-        `}
+        flex items-end md:items-center gap-4 ml-6 mr-6 md:mr-0 md:mt-4 mb-6 flex-col md:flex md:flex-row
+        ${isMenuOpen ? 'flex' : 'hidden'}
+      `}
         >
-            <div className='md:flex hidden gap-3'>
-                {imageData && <GatsbyImage image={imageData} alt='logo' />}
-                <MenuItem name='korczak.xyz' to='/' />
+            <div className="md:flex hidden gap-3">
+                {imageData && <GatsbyImage image={imageData} alt="logo" />}
+                <MenuItem name="korczak.xyz" to="/" />
             </div>
-            <div className='grow hidden md:block' />
+            <div className="grow hidden md:block" />
             {navigation.map(item => (
                 <MenuItem key={item.name} {...item} />
             ))}
-            <DarkModeSwitch className='hidden md:block' />
-            <div className='flex gap-3'>
+            <DarkModeSwitch className="hidden md:block" />
+            <div className="flex gap-3">
                 {languages.map(lng => (
                     <Link to={originalPath} language={lng} key={lng}>
                         <Flag lng={lng} />
@@ -51,6 +45,6 @@ const MainNavMenu: React.FC<MainNavMenuProps> = ({ navigation, isMenuOpen, langu
             </div>
         </nav>
     );
-}
+};
 
 export default MainNavMenu;
