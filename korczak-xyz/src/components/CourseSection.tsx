86b80851bea08f29698d074e01559dbc35d905e4
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-29 13:45:36 +0200

    refactor: Refactor CourseSection component rendering logic

--- korczak-xyz/src/components/CourseSection.tsx
+++ korczak-xyz/src/components/CourseSection.tsx
@@ -1,7 +1,7 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
-import React, { FC, useContext } from 'react';
+import React, { useContext } from 'react';
 
 import ThemeContext from '../context/ThemeContext';
 import { RedButton } from './RedButton';
@@ -10,14 +10,13 @@ interface CourseSectionProps {
   slug: string | null | undefined;
 }
 
-export const CourseSection: FC<CourseSectionProps> = ({ slug }) => {
-  if (!slug) {
-    return null;
-  }
+export const CourseSection: React.FC<CourseSectionProps> = ({ slug }) => {
+  if (!slug) return null;
+
   const { t } = useTranslation();
   const { theme } = useContext(ThemeContext);
 
-  const data = useStaticQuery<Queries.CourseSectionQuery>(graphql`
+  const data = useStaticQuery(graphql`
     query CourseSection {
       allMdx {
         nodes {
@@ -50,35 +49,28 @@ export const CourseSection: FC<CourseSectionProps> = ({ slug }) => {
     }
   `);
 
-  const course = data.allMdx.nodes.find(
-    node => node?.frontmatter?.slug === slug,
-  );
+  const nodes = data.allMdx?.nodes;
+  if (!nodes) return null;
 
-  if (!course?.frontmatter) {
-    return null;
-  }
+  const course = nodes.find((node: any) => node?.frontmatter?.slug === slug); // Used "any" for demonstration, replace with your specific type
+  if (!course?.frontmatter) return null;
 
   const { title, featuredImageColor, featuredImageBW, excerpt } =
     course.frontmatter;
+  if (!title || !excerpt) return null;
 
-  if (!title || !excerpt) {
-    return null; // Don't render the component if essential data is missing
-  }
+  const colorImage = getImage(
+    featuredImageColor?.childImageSharp?.gatsbyImageData,
+  );
+  const bwImage = getImage(featuredImageBW?.childImageSharp?.gatsbyImageData);
 
-  const colorImageData =
-    featuredImageColor?.childImageSharp?.gatsbyImageData || null;
-  const bwImageData = featuredImageBW?.childImageSharp?.gatsbyImageData || null;
-  const colorImage = getImage(colorImageData);
-  const bwImage = getImage(bwImageData);
+  const imageToDisplay = theme === 'dark' ? bwImage : colorImage;
+  if (!imageToDisplay) return null;
+
   return (
     <div className='flex flex-col gap-8'>
       <h3 className='text-4xl'>{title}</h3>
-      {theme === 'dark' && bwImage && (
-        <GatsbyImage image={bwImage} alt={title} />
-      )}
-      {theme === 'light' && colorImage && (
-        <GatsbyImage image={colorImage} alt={title} />
-      )}
+      <GatsbyImage image={imageToDisplay} alt={title} />
       <p>{excerpt}</p>
       <div>
         <RedButton text={t('Read more')} link={slug} />
